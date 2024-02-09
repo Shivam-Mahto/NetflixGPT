2ea3a5c2c969522b9047f43859b383224f3a0fc9
Shivam Mahto <shivammahto1202@gmail.com>
2024-02-09 18:43:59 +0530

    removed node modules

--- server/routes/gpt.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import getGPTSearch from "../controllers/gptController.js";
-const router = express.Router();
-
-router.post("/search", getGPTSearch);
-
-export default router;
